c01199adfe3d017da2e06da00a3fff6d5a7ec076
sasha <sasha.hlupin@gmail.com>
2022-08-09 16:26:45 +0300

    finally

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 from flask import Flask
 
-from coursework2_source.api.views import bluepint_api
-from coursework2_source.posts.views import post_blueprint
+from api.views import bluepint_api
+from posts.views import post_blueprint
 
 app = Flask(__name__)
 app.config['JSON_AS_ASCII'] = False
-app.config.from_pyfile("config.py")
+
 app.register_blueprint(post_blueprint)
 app.register_blueprint(bluepint_api)
 
--- tests/test_api.py
+++ tests/test_api.py
@@ -1,11 +1,7 @@
-import os
-from flask import Flask
-
 from coursework2_source import main
-from coursework2_source.main import app
+from coursework2_source.config import DATA_PATH_POSTS
 
 import pytest
-
 
 key_data = {"poster_name", "poster_avatar", "pic", "content", "views_count", "likes_count",
             "pk"}
@@ -13,27 +9,33 @@ key_data = {"poster_name", "poster_avatar", "pic", "content", "views_count", "li
 
 class Test_api:
 
+    @pytest.fixture
+    def app_instance(self):
+        app = main.app
+        app.config["DATA_PATH_POSTS"] = DATA_PATH_POSTS
+        test_client = app.test_client()
 
+        return test_client
 
-    def test_index(self):
-        result = app.test_client().get("/api", follow_redirects=True)
+    def test_index(self, app_instance):
+        result = app_instance.get("/api", follow_redirects=True)
 
         assert result.status_code == 200, "Неправильный статус "
 
-    def test_post_page(self,):
-        result = app.test_client().get("/api/posts", follow_redirects=True)
-        assert result.status_code==200,"Неправильный статус "
+    def test_post_page(self, app_instance):
+        result = app_instance.get("/api/posts", follow_redirects=True)
+        assert result.status_code == 200, "Неправильный статус "
 
-    def test_page_post_pk(self):
-        result = app.test_client().get("/api/posts/1", follow_redirects=True)
-        assert result.status_code==200
-        assert type(result.json)==dict,"несоответствующий тип данных"
+    def test_page_post_pk(self, app_instance):
+        result = app_instance.get("/api/posts/1", follow_redirects=True)
+        assert result.status_code == 200
+        assert type(result.json) == dict, "несоответствующий тип данных"
 
-    def test_page_found_name(self):
-        result = app.test_client().get("/api/posts/Sasha", follow_redirects=True)
-        assert result.status_code==404
+    def test_page_found_name(self, app_instance):
+        result = app_instance.get("/api/posts/Sasha", follow_redirects=True)
+        assert result.status_code == 404
 
-    def test_page_user(self):
-        result = app.test_client().get("/api/posts/Leo", follow_redirects=True)
-        assert result.status_code==200
-        assert type(result.json)==list,"несоответствующий тип данных"
+    def test_page_user(self, app_instance):
+        result = app_instance.get("/api/posts/Leo", follow_redirects=True)
+        assert result.status_code == 200
+        assert type(result.json) == list, "несоответствующий тип данных"
